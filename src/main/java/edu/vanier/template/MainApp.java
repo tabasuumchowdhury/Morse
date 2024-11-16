0171b5eb15623feb0f6111cd189333ab32ee9ccc
Tabasuum Chowdhury <150824316+tabasuumchowdhury@users.noreply.github.com>
2024-11-16 14:58:05 -0500

    Add files via upload

---
+++ src/main/java/edu/vanier/template/MainApp.java
@@ -0,0 +1,15 @@
+package edu.vanier.template;
+
+/**
+ * This is a JavaFX project template to be used for creating GUI applications.
+ * JavaFX 20.0.2 is already linked to this project in the build.gradle file.
+ * @link: https://openjfx.io/javadoc/22/
+ * @see: Build Scripts/build.gradle
+ * @author Frostybee.
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+}
